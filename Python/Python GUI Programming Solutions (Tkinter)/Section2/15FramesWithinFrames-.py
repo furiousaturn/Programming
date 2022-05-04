844fc62b4e44a47718b5414f104c67ec3d119952
Andrew Stephenson <amstephenson@burnley.ac.uk>
2022-05-04 14:02:02 +0100

    updates

--- Python/Python GUI Programming Solutions (Tkinter)/Section2/15FramesWithinFrames-.py
+++ Python/Python GUI Programming Solutions (Tkinter)/Section2/15FramesWithinFrames-.py
@@ -8,7 +8,7 @@ win.title("Python GUI")
 monty = ttk.LabelFrame(win, text="Monty Python")
 monty.grid(column=0, row=0)
 
-aLabel = ttk.Label(monty, text="A Label")
+aLabel = ttk.Label(monty, text=" ")
 aLabel.grid(column=0, row=0)
 
 
@@ -18,12 +18,13 @@ def clickMe():
 
 
 # Changing our label
-ttk.Label(monty, text="Enter a name:").grid(column=0, row=0)
+ttk.Label(monty, text="Enter a name:").grid(column=0, row=0, sticky="W")
 
 # Adding a textbox Entry Widget
 name = tk.StringVar()
 nameEntered = ttk.Entry(monty, width=12, textvariable=name)
-nameEntered.grid(column=0, row=1)
+nameEntered.grid(column=0, row=1, sticky=tk.W)
+
 
 # Adding a button
 action = ttk.Button(monty, text="Click Me!", command=clickMe)
